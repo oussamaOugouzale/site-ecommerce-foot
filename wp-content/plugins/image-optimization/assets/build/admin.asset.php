6da7a3a52d8d8b24587983279b05afc52ce082ff
oussama77A <oussagou49@gmail.com>
2024-12-20 22:13:02 +0100

    Initial commit - Ajout du projet WordPress e-commerce

--- wp-content/plugins/image-optimization/assets/build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '22905c77ce899fcbce7f');
--- wp-content/plugins/woocommerce/assets/client/admin/app/style.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('version' => '756b2ed79958f6462350');
